3682b0f61a8bb93f30a6830eda2cf15ba69fe00c
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2024-09-10 17:24:00 +0200

    Ajout panier, début menu rapide
    Implémentation et ajout d'une page de panier, qui enregistre la sélection d'achats de l'utilisateur, avec possibilité d'incrémenter ou de décrémenter le nombre de meubles achetés, ou de les retirer. Le bouton de retour en arrière fonctionne correctement maintenant, et il y a maintenant option de revenir au menu directement avec le bouton du milieu en bas de l'écran.

--- Assets/Scripts/Controllers/NavigationUIController.cs
+++ Assets/Scripts/Controllers/NavigationUIController.cs
@@ -10,6 +10,8 @@ public class NavigationUIController : MonoBehaviour
     [SerializeField]
     VisualTreeAsset furnitureNavigationTemplate;
     [SerializeField]
+    VisualTreeAsset cartFurnitureDisplayTemplate;
+    [SerializeField]
     Camera navigationCamera;
 
     static NavigationUIController _this;
@@ -18,6 +20,7 @@ public class NavigationUIController : MonoBehaviour
     ApplicationManager manager;
     List<UIDocument> documentHistory;
     FurnitureSO displayedFurniture;
+    Dictionary<FurnitureSO, int> userCart;
 
     private void Awake()
     {
@@ -33,7 +36,7 @@ public class NavigationUIController : MonoBehaviour
         currentDocument = documentsReferences[0];
         manager = GameObject.FindObjectOfType<ApplicationManager>();
         documentHistory = new List<UIDocument>();
-        documentHistory.Add(currentDocument);
+        userCart = new Dictionary<FurnitureSO, int>();
 
         VisualElement docRoot = currentDocument.rootVisualElement;
         docRoot.Q<Button>("ShopButton").clicked += GoToShop;
@@ -42,9 +45,9 @@ public class NavigationUIController : MonoBehaviour
 
     private void GoBack()
     {
-        if (documentHistory.Count > 1)
+        if (documentHistory.Count > 0)
         {
-            ChangeNavigationDocument(documentHistory[documentHistory.Count - 1]);
+            ChangeNavigationDocument(documentHistory[documentHistory.Count - 1], true);
             documentHistory.RemoveAt(documentHistory.Count - 1);
         }
         else
@@ -53,13 +56,16 @@ public class NavigationUIController : MonoBehaviour
         }
     }
 
-    private void ChangeNavigationDocument(UIDocument newPage)
+    private void ChangeNavigationDocument(UIDocument newPage, bool isGoBack)
     {
         newPage.gameObject.SetActive(true);
         VisualElement newRoot = newPage.rootVisualElement;
         newRoot.Q<Button>("BackButton").clicked += GoBack;
+        newRoot.Q<Button>("HomeButton").clicked += GoToHome;
 
-        documentHistory.Add(currentDocument);
+        if (!isGoBack)
+            documentHistory.Add(currentDocument);
+
         currentDocument.gameObject.SetActive(false);
         currentDocument = newPage;
 
@@ -89,18 +95,28 @@ public class NavigationUIController : MonoBehaviour
             case "FurnitureDetailsDocument":
                 newRoot.Q<VisualElement>("TitleContainer").Q<Label>().text = displayedFurniture.name;
                 newRoot.Q<VisualElement>("Preview").style.backgroundImage = new StyleBackground(displayedFurniture.preview);
-                newRoot.Q<Label>("Dimensions").text = "Dimensions : " + displayedFurniture.width + " x " + displayedFurniture.length + " x " + displayedFurniture.height;
+                newRoot.Q<Label>("Dimensions").text = "Dimensions : " + displayedFurniture.width + "m x " + displayedFurniture.length + "m x " + displayedFurniture.height + "m";
                 newRoot.Q<Label>("Description").text = displayedFurniture.description;
                 newRoot.Q<Label>("PriceTag").text = "Prix : " + displayedFurniture.price + " €";
+
+                newRoot.Q<Button>("AddToCart").clicked += AddToCart;
+                break;
+            case "ShopCartDocument":
+                DrawCartScrollView();
                 break;
             default:
                 break;
         }
     }
 
+    private void GoToHome()
+    {
+        ChangeNavigationDocument(documentsReferences[0], false);
+    }
+
     private void GoToShop()
     {
-        ChangeNavigationDocument(documentsReferences[1]);
+        ChangeNavigationDocument(documentsReferences[1], false);
     }
 
     private void GoToARTest()
@@ -113,12 +129,75 @@ public class NavigationUIController : MonoBehaviour
     private void GoToFurnitureDetails(FurnitureSO furniture)
     {
         displayedFurniture = furniture;
-        ChangeNavigationDocument(documentsReferences[2]);
+        ChangeNavigationDocument(documentsReferences[2], false);
     }
 
     public static void ReenableNavigation()
     {
         currentDocument.gameObject.SetActive(true);
         _this.navigationCamera.gameObject.SetActive(true);
+    }
+
+    private void AddToCart()
+    {
+        if (userCart.ContainsKey(displayedFurniture))
+        {
+            userCart[displayedFurniture]++;
+        }
+        else
+        {
+            userCart.Add(displayedFurniture, 1);
+        }
+        ChangeNavigationDocument(documentsReferences[3], false);
+    }
+
+    private void IncrementFurnitureCart(FurnitureSO furniture)
+    {
+        userCart[furniture]++;
+        DrawCartScrollView();
+    }
+
+    private void DecrementFurnitureCart(FurnitureSO furniture)
+    {
+        if (userCart[furniture] > 1)
+        {
+            userCart[furniture]--;
+            DrawCartScrollView();
+        }
+    }
+
+    private void RemoveFurnitureCart(FurnitureSO furniture)
+    {
+        userCart.Remove(furniture);
+        DrawCartScrollView();
+    }
+
+    private void DrawCartScrollView()
+    {
+        ScrollView cartDisplay = currentDocument.rootVisualElement.Q<ScrollView>("Cart");
+
+        cartDisplay.Clear();
+
+        float totalPrice = 0f;
+
+        foreach (KeyValuePair<FurnitureSO, int> item in userCart)
+        {
+            VisualElement cartFurniture = cartFurnitureDisplayTemplate.CloneTree().Q<VisualElement>(null, new string[] { "CartItem" });
+
+            cartFurniture.Q<VisualElement>(className: "FurniturePreview").style.backgroundImage = new StyleBackground(item.Key.preview);
+            cartFurniture.Q<Label>(className: "FurnitureName").text = item.Key.name;
+            cartFurniture.Q<Label>(className: "Amount").text = item.Value.ToString();
+            cartFurniture.Q<Label>(className: "Price").text = item.Key.price + " €";
+
+            totalPrice += item.Key.price * item.Value;
+
+            cartFurniture.Q<Button>(className: "ReduceQuantity").clicked += () => DecrementFurnitureCart(item.Key);
+            cartFurniture.Q<Button>(className: "AugmentQuantity").clicked += () => IncrementFurnitureCart(item.Key);
+            cartFurniture.Q<Button>(className: "Remove").clicked += () => RemoveFurnitureCart(item.Key);
+
+            cartDisplay.Add(cartFurniture);
+        }
+
+        currentDocument.rootVisualElement.Q<Label>("TotalPrice").text = "Total : " + totalPrice + " €";
     }
 }
